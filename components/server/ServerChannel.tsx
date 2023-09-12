238eef6f1c14b588ec8bcd6cbb8d7219bed633c7
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-09-12 13:03:02 +0600

    delete channels ui setup and some other fix

--- components/server/ServerChannel.tsx
+++ components/server/ServerChannel.tsx
@@ -5,6 +5,7 @@ import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
 import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import ActionTooltip from "../ActionTooltip";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -19,6 +20,7 @@ const iconMap = {
 };
 
 const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
+  const { onOpen } = useModal();
   const params = useParams();
   const router = useRouter();
 
@@ -50,8 +52,11 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
             <Edit className="h-4 w-4 hidden group-hover:block text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
           </ActionTooltip>
 
-          <ActionTooltip label="Edit">
-            <Trash className="h-4 w-4 hidden group-hover:block text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+          <ActionTooltip label="Delete">
+            <Trash
+              onClick={() => onOpen("deleteChannel", { server, channel })}
+              className="h-4 w-4 hidden group-hover:block text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+            />
           </ActionTooltip>
         </div>
       )}
--- components/server/ServerSearch.tsx
+++ components/server/ServerSearch.tsx
@@ -75,7 +75,7 @@ const ServerSearch = ({ data }: ServerSearchProps) => {
           Search
         </p>
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
-          <span className="text-xs">Ctrl</span>K
+          Ctrl K
         </kbd>
       </button>
       <CommandDialog open={open} onOpenChange={setOpen}>
